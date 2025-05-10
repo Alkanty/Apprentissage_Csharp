a06772ebe2140865bdc1127510867f31f05e711b
Alkanty <kyriann-pro@hotmail.fr>
2025-05-10 14:29:11 +0200

    exo 1

--- ConfigTest.cs
+++ ConfigTest.cs
@@ -1,34 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace Apprentissage_Csharp
 {
     public class ConfigTest
     {
 
+        public static void CastingAndConversionExo2()
+        {
+
+
+
+
+
+
+        }
+
+
+        public static void CastingAndConversionExo1()
+        {
+
+            //  Create a looping structure that can be used to iterate through each string value in the array values.
+            //  https://learn.microsoft.com/en-us/training/modules/csharp-convert-cast/4-challenge
+            // Rule 1: If the value is alphabetical, concatenate it to form a message.
+            //Rule 2: If the value is numeric, add it to the total. 
+            //Rule 3: The result should match the following output: Message: ABCDEF Total: 68.3
+
+            string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+            float numberadd = 0;
+            string wordadd = "";
+
+
+            foreach (string value in values)
+
+            {
+                if (float.TryParse(value, out float result))
+                {
+                    Console.WriteLine("DEBUG:adding it to the rest of the numbers");
+                    numberadd += result;
+                }
+                else
+                {
+                    Console.WriteLine("DEBUG: adding it to the rest of the words");
+                    wordadd += value;
+                }
+            }
+            Console.WriteLine($"Message:{wordadd} \nTotal :{numberadd}");
+
+
+        }
+
 
         public static void CastingAndConversion()
-        {
-            int first = 2;
-            string second = "4";
-            int result = first + Convert.ToInt16(second);        // Convert.class tha able to convert multiple format str to int
-            Console.WriteLine(result);
+        { 
+            //int first = 2;
+            //string second = "4";
+            //int result = first + Convert.ToInt16(second);        // Convert.class tha able to convert multiple format str to int
+            //Console.WriteLine(result);                           // better for ROUNDING
 
-            decimal myDecimal = 3.14m;
-            Console.WriteLine($"decimal: {myDecimal}");             
+            //decimal myDecimal = 3.14m;
+            //Console.WriteLine($"decimal: {myDecimal}");             
 
-            int myInt = (int)myDecimal;                           // cast dec > int = trunk the number to only 3 . Narrowing conversion
-            Console.WriteLine($"int: {myInt}");                   // Casting is an instruction to the C# compiler that you know precision may be lost, but you're willing to accept it.
-                                                                  // BEWARE OF THE TRUNCATED
+            //int myInt = (int)myDecimal;                           // cast dec > int = trunk the number to only 3 . Narrowing conversion
+            //Console.WriteLine($"int: {myInt}");                   // Casting is an instruction to the C# compiler that you know precision may be lost, but you're willing to accept it.
+            // BEWARE OF THE TRUNCATED
 
-            string first1 = "5";
-            string second2 = "7";
-            int sum = int.Parse(first1) + int.Parse(second);      // Parse() method, which converts a string into the given data type. In this case
-            Console.WriteLine(sum);
+            //string first1 = "5";
+            //string second2 = "7";
+            //int sum = int.Parse(first1) + int.Parse(second);      // Parse() method, which converts a string into the given data type. In this case
+            //Console.WriteLine(sum);
+
+
+
+
+
+            //string value = "102";                                   // Parse() If successful, it stores the converted value in an out paramete It returns a bool to indicate whether the action succeeded or failed
+            //int result = 0;
+
+            //if (int.TryParse(value, out result))
+            //{
+            //    Console.WriteLine($"Measurement: {result}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Unable to report the measurement.");
+            //}
+            //Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
         }
         public static void ValueTypeLimit()
